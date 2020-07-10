3fa1a592882579c8f1c51d4bfdbeda011a4966ac
personalfebus <gleb89206535210@gmail.com>
2020-07-10 15:43:59 +0300

    Added simple server and client

--- client/main.cpp
+++ client/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 #include <asio.hpp>
+#include <vector>
 
-using namespace std;
 using asio::ip::tcp;
 
-int main()
-{
-	cout << "sasalovo";
+int main() {
+	try {
+		asio::io_context io_context;
+		asio::steady_timer t(io_context, asio::chrono::seconds(5));
+		tcp::resolver resolver(io_context);
+		tcp::resolver::results_type endpoints =
+			resolver.resolve("127.0.0.1", "daytime");
+		tcp::socket socket(io_context);
+		asio::connect(socket, endpoints);
+		int iter = 0;
+		for (;;) {
+			if (iter == 101) {
+				break;
+			}
+			iter++;
+			//std::cout << "g";
+			//asio::streambuf read_buffer;
+			//asio::error_code error;
+
+			//if (error == asio::error::eof)
+				//break; // Connection closed cleanly by peer.
+			//else if (error)
+				//throw asio::system_error(error); // Some other error.
+
+			if (iter % 25 == 0) {
+				std::string message = "ALO\n";
+				asio::error_code ignored_error;
+				asio::write(socket, asio::buffer(message), ignored_error);
+				t.wait();
+			}	
+		}
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "Ger Nick";
 	return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <asio.hpp>
+#include <vector>
 
-using namespace std;
 using asio::ip::tcp;
 
-int main()
-{
-	cout << "aee";
-	asio::io_context io_context;
-	tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8014));
-	tcp::socket socket(io_context);
-	acceptor.accept(socket);
-	cout << "sasalovo";
+int main() {
+	try {
+		std::cout << "ayy\n";
+		asio::io_context io_context;
+		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+		tcp::socket socket(io_context);
+		acceptor.accept(socket);
+		int time = 0;
+
+		for (;;) {
+			time++;
+
+			std::vector<char> buf(128);
+			asio::error_code error;
+
+			size_t len = socket.read_some(asio::buffer(buf), error);
+
+			if (error != asio::error::eof && error) {
+				std::cout << error;
+				throw asio::system_error(error);
+			}
+			//std::cout.write(buf.data(), len);
+
+			std::string str(buf.data());
+			if (str != "") {
+				std::cout << time << std::endl;
+			}
+
+			std::cout << str;
+		}
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "\n-end-\n";
 	return 0;
 }
